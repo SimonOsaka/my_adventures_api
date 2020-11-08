550de55aa4c3737b438b6afc9042891ba098fc3a
许忠亮 <geniusmickymouse@qq.com>
2020-11-08 17:44:43 +0800

    升级版本

--- src/handlers/adventures/tabs.rs
+++ src/handlers/adventures/tabs.rs
@@ -9,18 +9,18 @@ pub async fn tabs_adventures(token: Option<String>) -> Result<impl warp::Reply, 
             name: "全部".to_owned(),
             item_id: 0,
         },
-        Tabs {
-            name: "日常".to_owned(),
-            item_id: 1,
-        },
+        // Tabs {
+        //     name: "日常".to_owned(),
+        //     item_id: 1,
+        // },
         Tabs {
             name: "搞笑".to_owned(),
             item_id: 2,
         },
-        Tabs {
-            name: "游戏".to_owned(),
-            item_id: 3,
-        },
+        // Tabs {
+        //     name: "游戏".to_owned(),
+        //     item_id: 3,
+        // },
         Tabs {
             name: "影视".to_owned(),
             item_id: 4,
