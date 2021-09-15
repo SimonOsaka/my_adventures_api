4e3e00cbc10038a8b43c9fdcf1d57a2961dc47ed
Simon Osaka <geniusmickymouse@qq.com>
2021-09-15 12:22:25 +0800

    update flexi_log version

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // It has something to do with with the routes.
 #![type_length_limit = "1422483"]
 
-use flexi_logger::{Age, Cleanup, Criterion, Duplicate, LogTarget, Logger, Naming};
+use flexi_logger::{Age, Cleanup, Criterion, Duplicate, FileSpec, Logger, Naming, WriteMode};
 #[macro_use]
 extern crate log;
 
@@ -18,14 +18,15 @@ mod routes;
 async fn main() {
     dotenv::dotenv().ok();
 
-    Logger::with_env()
-        .log_target(LogTarget::File)
-        .log_to_file()
-        .directory("/apps/log/rust")
+    Logger::try_with_env()
+        .unwrap()
+        .log_to_file(FileSpec::default().directory("/apps/log/rust"))
+        .write_mode(WriteMode::BufferAndFlush)
         .print_message()
         .duplicate_to_stdout(Duplicate::All)
         .duplicate_to_stderr(Duplicate::Warn)
         .format(crate::logger::logger_format)
+        // .set_palette(("196;208;34;21;8").to_string())
         .rotate(Criterion::Age(Age::Day), Naming::Timestamps, Cleanup::KeepLogFiles(7))
         .append()
         .start()
